8c96bd118769e064eebbd0989a700223604a8174
Johnaits <98190892+Johnaits@users.noreply.github.com>
2022-07-26 16:52:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
-for (var i = 0;i<document.querySelectorAll(".drum").length;i++){
+for (var i = 0;i<document.querySelectorAll(".drum").length;i++)
+{
     document.querySelectorAll("button")[i].addEventListener("click",function(){
-        var audio = new Audio("sounds/tom-1.mp3");
+        
+        switch (this.innerHTML) {
+            case "w":
+                var crash = new Audio(sounds/crash.mp3);
+                crash.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "a":
+                var kick = new Audio(sounds/kick.mp3);
+                kick.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "s":
+                var snare = new Audio(sounds/snare.mp3);
+                snare.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "d":
+                var tom1 = new Audio(sounds/tom-1.mp3);
+                tom1.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "j":
+                var tom2 = new Audio(sounds/tom-2.mp3);
+                tom2.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "k":
+                var tom3 = new Audio(sounds/tom-3.mp3);
+                tom3.play();
+                //this.style.color = "white"; 
+                break;
+
+            case "l":
+                var tom4 = new Audio(sounds/tom-4.mp3);
+                tom4.play();
+                //this.style.color = "white"; 
+                break;
+            default:console.log(button.innerHTML);
+                break;
+        }
+
+
+
+
+
+        /* switch (this.innerHTML) 
+        {
+            case "w":
+                var pathSound = "sounds/crash.mp3";
+                alert(path);
+                break;
+        
+            default:
+                break;
+        }
+        var audio = new Audio(pathSound);
         audio.play();
+        this.style.color = "white"; */
+
     });
    
 }
