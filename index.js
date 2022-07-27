f922bac7ecc7d2a9c84c4ed03b48ea6df0273b01
Johnaits <johnykrc@gmail.com>
2022-07-27 00:00:55 -0300

    second commit

--- index.js
+++ index.js
@@ -1,62 +1,39 @@
-for (var i = 0;i<document.querySelectorAll(".drum").length;i++)
+document.addEventListener("keydown",function(){
+    alert(this.innerHTML);
+}); 
+
+for (var i = 0;i<document.querySelectorAll("button").length;i++)
 {
     document.querySelectorAll("button")[i].addEventListener("click",function(){
-        
-        switch (this.innerHTML) {
+
+        switch (this.innerHTML) 
+        {
             case "w":
-                var crash = new Audio(sounds/crash.mp3);
-                crash.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/crash.mp3");
                 break;
 
             case "a":
-                var kick = new Audio(sounds/kick.mp3);
-                kick.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/kick-bass.mp3");
                 break;
 
             case "s":
-                var snare = new Audio(sounds/snare.mp3);
-                snare.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/snare.mp3");
                 break;
 
             case "d":
-                var tom1 = new Audio(sounds/tom-1.mp3);
-                tom1.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/tom-1.mp3");
                 break;
 
             case "j":
-                var tom2 = new Audio(sounds/tom-2.mp3);
-                tom2.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/tom-2.mp3");
                 break;
 
             case "k":
-                var tom3 = new Audio(sounds/tom-3.mp3);
-                tom3.play();
-                //this.style.color = "white"; 
+                var pathSound = ("sounds/tom-3.mp3");
                 break;
-
+                
             case "l":
-                var tom4 = new Audio(sounds/tom-4.mp3);
-                tom4.play();
-                //this.style.color = "white"; 
-                break;
-            default:console.log(button.innerHTML);
-                break;
-        }
-
-
-
-
-
-        /* switch (this.innerHTML) 
-        {
-            case "w":
-                var pathSound = "sounds/crash.mp3";
-                alert(path);
+                var pathSound = ("sounds/tom-4.mp3");
                 break;
         
             default:
@@ -64,15 +41,11 @@ for (var i = 0;i<document.querySelectorAll(".drum").length;i++)
         }
         var audio = new Audio(pathSound);
         audio.play();
-        this.style.color = "white"; */
+        //this.style.color = "white";
 
     });
    
 }
-
-
-
-
 
 
 
